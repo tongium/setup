ca6d54b010f839994e0a4a115073faaa18ffc56f
Warinthorn Tuadsin <mr.gnot@gmail.com>
2022-06-19 14:32:48 +0700

    updated steps

--- mac-setup.sh
+++ mac-setup.sh
@@ -4,22 +4,36 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cli=(
+    git
     go
     node
     helm
     kubectl
     wrk
+    git-flow
+    nmap
+    http-server
 )
 
 brew install ${cli[@]}
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+touch ~/.zshenv
+
+curl -o /usr/local/bin/cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
+chmod +x /usr/local/bin/cloud_sql_proxy
 
 apps=(
     google-chrome
     visual-studio-code
     warp
+    postman
     fork
     docker
+    ngrok
     google-cloud-sdk
+    dbeaver-community
+    lens
 )
 
 brew install --cask ${apps[@]}
